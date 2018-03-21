482cd0b334d5645bf2c2097bff51e32274dd1edd
Vinícius Augutis <vinicius_augutis@hotmail.com>
2018-03-21 19:33:12 -0300

    Criando estrutura do projeto a partir do problema 1

---
+++ README.md
@@ -0,0 +1,2 @@
+# arquitetura-problema1
+Resolução do problema 1 de arquitetura de software
---
+++ src/service/ClienteService.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Cliente;
+
+/**
+ *
+ * @author emerson
+ */
+public class ClienteService {
+    
+    private List<Cliente> listaClientes = new ArrayList<Cliente>();
+    
+    public void adicionarCliente(Cliente cliente) {
+        getListaClientes().add(cliente);
+        System.out.println(this.listaClientes);
+    }
+    
+    public List<Cliente> getListaClientes() {
+        return listaClientes;
+    }
+    
+}
